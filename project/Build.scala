30944aed3c2515e32aa8a5181e4468a192d32ddd
Seung Lee <pythonmania@gmail.com>
2012-06-28 01:15:19 +0900

    initial import

---
+++ project/Build.scala
@@ -0,0 +1,16 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+  val appName = "wikidraw"
+  val appVersion = "1.0"
+
+  val appDependencies = Seq( // Add your project dependencies here,
+  )
+
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings( // Add your own project settings here      
+  )
+
+}

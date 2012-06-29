bde08c8c8dab931e691ac4a314624018526b240c
seung lee <pythonmania@gmail.com>
2012-06-29 15:50:47 +0900

    due to cross site request forgery, get issues from server

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,10 +2,16 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import dispatch._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.main())
+  }
+
+  def githubIssues() = Action {
+    val res = Http(url("https://api.github.com/repos/pythonmania/wikidraw/issues") as_str)
+    Ok(res).as(JSON)
   }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,8 @@ object ApplicationBuild extends Build {
   val appName = "wikidraw"
   val appVersion = "1.0"
 
-  val appDependencies = Seq( // Add your project dependencies here,
+  val appDependencies = Seq( 
+    "net.databinder" %% "dispatch-http" % "0.8.8"
   )
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings( // Add your own project settings here      
